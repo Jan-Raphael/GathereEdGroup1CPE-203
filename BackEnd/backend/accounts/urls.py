c81115f04019634c475b43a7737ea758b460a7fd
Hendy-Tona <generic253@gmail.com>
2025-02-11 22:27:06 +0800

    Updated Django settings and applied migrations for the BackEnd

--- BackEnd/backend/accounts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import UserCreateView, UserProfileView
-
-urlpatterns = [
-    path('register/', UserCreateView.as_view(), name='register'),
-    path('profile/', UserProfileView.as_view(), name='profile'),
-]
